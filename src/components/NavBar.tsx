6da1203a7e1a75cf10c00cb55f37ad6e4e42fba6
CodeMarcin <m.zaczkowski@o2.pl>
2022-03-15 18:38:50 +0100

    Initial

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,8 @@
+import styles from './NavBar.module.css';
+export const NavBar = () => {
+  return (
+    <div className={styles['navbar']}>
+      <div className={styles['navbar__wrapper']}>NavBar</div>
+    </div>
+  );
+};
